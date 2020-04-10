b951726e14994bcb186d17221195298d63b5334c
nhamhung <nhamhung.gttn@gmail.com>
2020-04-10 13:09:02 +0800

    Add tests for edit resume command/descriptor and skill command/descriptor

--- src/test/java/seedu/address/logic/commands/edit/EditSkillDescriptorTest.java
+++ src/test/java/seedu/address/logic/commands/edit/EditSkillDescriptorTest.java
@@ -1,4 +1,57 @@
 package seedu.address.logic.commands.edit;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.item.Skill;
+import seedu.address.testutil.SkillBuilder;
+
 public class EditSkillDescriptorTest {
+
+    private Skill sampleSkill = new SkillBuilder().build();
+
+    private EditSkillDescriptor editSkillDescriptor = new EditSkillDescriptor();
+
+    @Test
+    public void equals() {
+        editSkillDescriptor.setName(sampleSkill.getName());
+        editSkillDescriptor.setTags(sampleSkill.getTags());
+        editSkillDescriptor.setLevel(sampleSkill.getLevel());
+
+        EditSkillDescriptor editSkillDescriptorToTest =
+                new EditSkillDescriptor(editSkillDescriptor);
+
+        // same name -> returns true
+        assertEquals(editSkillDescriptor.getName().get(), sampleSkill.getName());
+
+        // same object -> returns true
+        assertEquals(editSkillDescriptor.getTags().get(), sampleSkill.getTags());
+
+        // same name -> returns true
+        assertEquals(editSkillDescriptor.getLevel().get(), sampleSkill.getLevel());
+
+        // same name -> returns true
+        assertEquals(editSkillDescriptorToTest.getName().get(), editSkillDescriptor.getName().get());
+
+        // same object -> returns true
+        assertEquals(editSkillDescriptorToTest.getTags().get(), editSkillDescriptor.getTags().get());
+
+        // same object -> returns true
+        assertEquals(editSkillDescriptorToTest.getLevel().get(), editSkillDescriptor.getLevel().get());
+
+        // same descriptor object -> returns true
+        assertEquals(editSkillDescriptor, editSkillDescriptorToTest);
+
+        // checks two descriptors if same -> returns true
+        assertTrue(editSkillDescriptor.equals(editSkillDescriptorToTest));
+    }
+
+    @Test
+    public void field_isAnyNonNull_falseIfAllNull() {
+        EditSkillDescriptor editSkillDescriptor = new EditSkillDescriptor();
+        assertEquals(editSkillDescriptor.isAnyFieldEdited(), false);
+    }
 }
+
